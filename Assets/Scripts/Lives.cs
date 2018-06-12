58bdbc5c98e1f5fbfb2bd44f41ac7c0815ebf802
AlexanderLoo <AlexanderLoo>
2018-06-12 09:47:11 -0500

    Old Retro Racing

---
+++ Assets/Scripts/Lives.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Lives : MonoBehaviour {
+
+	public int maxLife=3;
+
+	void Awake(){
+
+		if (PlayerPrefs.GetInt ("FirstTime") == 0) {
+			PlayerPrefs.SetInt ("FirstTime", 1);
+			SetLivesFirstTime ();
+		}
+	}
+
+	//Esta función se usa la primera vez que se entra al juego
+	public void SetLivesFirstTime(){
+
+		SetCurrentLives (maxLife);
+	}
+
+	//Función para agregar o remover vida(dependiendo del parámetro; si es positivo se agrega una vida, si es negativo se remueve)
+	public void LivesManager(int i){
+
+		SetCurrentLives (GetCurrentLives() + i);
+	}
+
+	//Guarda en la memoria la cantidad de vida que nos queda donde i es el valor a guardar
+	public void SetCurrentLives(int i){
+
+		PlayerPrefs.SetInt ("CurrentLife", i);
+	}
+	//Retorna la cantidad de vida guardada en la memoria
+	public int GetCurrentLives(){
+
+		return PlayerPrefs.GetInt ("CurrentLife");
+	}
+}
